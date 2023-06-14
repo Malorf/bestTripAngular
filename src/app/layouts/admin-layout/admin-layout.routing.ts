936d7a3428b411489a34c3c6c66b98f6ebf97fe6
Malorf <cedric.rahbari@gmail.com>
2023-06-14 10:18:51 +0200

    rectifDashboard

--- src/app/layouts/admin-layout/admin-layout.routing.ts
+++ src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -5,17 +5,16 @@ import { IconsComponent } from "../../pages/icons/icons.component";
 import { MapComponent } from "../../pages/map/map.component";
 import { UserComponent } from "../../pages/user/user.component";
 import { TypographyComponent } from "../../pages/typography/typography.component";
-import { TransportsComponent } from "src/app/pages/transport/transport.component";
+import { TransportsComponent } from "../../pages/transport/transport.component";
 import { PlaceComponent } from "../../pages/place/place.component";
 import { HomeComponent } from "../../pages/home/home.component";
-import { LoginComponent } from "src/app/pages/login/login.component";
+import { LoginComponent } from "../../pages/login/login.component";
 
 
 export const AdminLayoutRoutes: Routes = [
 
   { path: "experience", component: ExperienceComponent },
   { path: "home", component: HomeComponent },
-  { path: "dashboard", component: DashboardComponent },
   { path: "icons", component: IconsComponent },
   { path: "maps", component: MapComponent },
   { path: "user", component: UserComponent },
